f6eb890b58fadcaba3efd6882bb6bdb451828131
liliumbaker <43766263+liliumbaker@users.noreply.github.com>
2018-10-02 21:39:33 -0300

    Add files via upload

---
+++ nt1-fullstack-02-fundamentos/JS/exemplo-3-gastos/script/script.js
@@ -0,0 +1,29 @@
+const button = document.querySelector(".transacao__button");
+button.addEventListener("click", function(event){
+    event.preventDefault();
+
+    const tabela = document.querySelector(".extrato__table");
+    const inputName = document.querySelector("#transacaoInputName");
+    const inputMoney = document.querySelector("#transacaoInputMoney");
+    const inputData = document.querySelector("#transacaoInputDate");
+
+    const linha = document.createElement("tr");
+
+    const colunaNome = document.createElement("td");
+    const colunaNomeInput = document.createTextNode(inputName.value);
+    colunaNome.appendChild(colunaNomeInput);
+
+    const colunaMoney = document.createElement("td");
+    const colunaMoneyInput = document.createTextNode(inputMoney.value);
+    colunaMoney.appendChild(colunaMoneyInput);
+
+    const colunaData = document.createElement("td");
+    const colunaDataInput = document.createTextNode(inputData.value);
+    colunaData.appendChild(colunaDataInput);
+
+    linha.appendChild(colunaNome);
+    linha.appendChild(colunaMoney);
+    linha.appendChild(colunaData);
+    tabela.appendChild(linha);
+
+});
